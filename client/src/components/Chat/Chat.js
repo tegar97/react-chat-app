a2b44a257c1aed115fd871dce887852334f6a788
Muhammad Tegar Akmal <eategar@gmail.com>
2021-01-29 14:31:46 +0700

    Connect to socket

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,16 @@
-import React from 'react'
-
-function Chat() {
+import React,{useEffect,useState} from 'react'
+import queryString from 'query-string'
+import io from 'socket.io-client'
+function Chat({location}) {
+    const [name,setName] = useState('')
+    const [room,setRoom] = useState('')
+    const ENDPOINT = 'http://localhost:5000/'
+    useEffect(() =>{
+        const {name,room } = queryString.parse(location.search);
+        const socket = io(ENDPOINT)
+        setName(name)
+        setRoom(name)
+    },[ENDPOINT,location.search])
     return (
         <div>
             <h1>CHAT</h1>
--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const socketIo = require('socket.io')
 const http = require('http');
-
+const cors = require('cors')
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server)
+const io = socketIo(server,{
+    cors:{
+        origin: '*',
+        methods: ["GET", "POST"],
+        credentials: true
 
+    }
+})
 const router = require('./router');
-
+app.use(cors())
 io.on('connection',(socket =>{
     console.log('we have a new connection')
     socket.on('disconnect',() =>{
