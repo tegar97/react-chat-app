b427eb9b4d953f15349daae117f3aba73c4d193f
Muhammad Tegar Akmal <eategar@gmail.com>
2021-01-29 15:37:21 +0700

    socket on message

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -3,14 +3,27 @@ import queryString from 'query-string'
 import io from 'socket.io-client'
 function Chat({location}) {
     const [name,setName] = useState('')
-    const [room,setRoom] = useState('')
+    const [room,setRoom] = useState('');
+    const [messages,setMessages] = useState('')
     const ENDPOINT = 'http://localhost:5000/'
     useEffect(() =>{
         const {name,room } = queryString.parse(location.search);
         const socket = io(ENDPOINT)
         setName(name)
         setRoom(name)
+        socket.emit('join',{name, room})
+
+        return () => {
+            socket.emit('disconnect')
+            socket.off();
+        } 
     },[ENDPOINT,location.search])
+
+    useEffect(() => {
+        socket.on('message',(message) =>{
+            setMessages([...messages,message])
+        })
+    },[messages])
     return (
         <div>
             <h1>CHAT</h1>
--- server.js
+++ server.js
@@ -4,18 +4,36 @@ const http = require('http');
 const cors = require('cors')
 const app = express();
 const server = http.createServer(app);
+const  {addUser,removeUser,getUser,getUsersinRoom} = require('./users')
 const io = socketIo(server,{
     cors:{
         origin: '*',
         methods: ["GET", "POST"],
         credentials: true
-
     }
 })
 const router = require('./router');
 app.use(cors())
 io.on('connection',(socket =>{
     console.log('we have a new connection')
+    socket.on('join',({name,room},callback) => {
+      const {error,user} = addUser({id : socket.id,name,room});
+
+      if(error) return callback(error)
+
+      socket.emit('message',{user:'admin',text: `${user.name},welcome to the room ${user.room}`})
+      socket.broadcast.to(user.room).emit('message',{user : 'admin',text : `${user.name},has joined!`})
+      
+    
+      socket.join(user.room);
+      callback()
+    })
+
+    socket.on("sendMessage",(message,callback) =>{
+        const user = getUser(socket.id);
+        io.to(user.room).emit('message',{user:user.name,text: message});
+        callback()
+    })
     socket.on('disconnect',() =>{
         console.log('user has left')
     })
